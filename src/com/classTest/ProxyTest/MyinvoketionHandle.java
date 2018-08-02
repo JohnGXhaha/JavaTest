ea238372317495cc53a834353a1e595cbe7d0b0d
JonnGXhaha <376419326@qq.com>
2018-08-02 23:30:24 +0800

    动态代理练习注解添加

--- src/com/classTest/ProxyTest/MyinvoketionHandle.java
+++ src/com/classTest/ProxyTest/MyinvoketionHandle.java
@@ -7,7 +7,7 @@ public class MyinvoketionHandle implements InvocationHandler{
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		System.out.println("执行的方法为"+method.getName());
+		System.out.println("执行的方法为"+method);
 		
 		if(args!=null) {
 			for(Object val:args) {
--- src/com/classTest/ProxyTest/ProxyTest.java
+++ src/com/classTest/ProxyTest/ProxyTest.java
@@ -1,15 +1,17 @@
 package com.classTest.ProxyTest;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
-
+//动态代理
 public class ProxyTest {
 	public static void main(String[] args) {
-		MyinvoketionHandle invoketionHandle=new MyinvoketionHandle();
-		
+		//创建InvocationHandler对象
+		InvocationHandler invoketionHandle=new MyinvoketionHandle();
+		//用InvocationHandler创建动态代理对象
 		Person p=(Person)Proxy.newProxyInstance(Person.class.getClassLoader(), new Class[]{Person.class}, invoketionHandle);
 		
 		p.say("你好");
 		p.walk();
-		
+			
 	}
 }
